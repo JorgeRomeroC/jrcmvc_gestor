18ef91c54cb1d0b05d628bdc3e0329b15e5b07eb
jorgeromeroc <jorgeromeroc.12@gmail.com>
2022-07-20 16:16:59 -0400

    Creada tabla de roles en la base de datos, creado el controlador de roles y la vista correspondiente.

---
+++ Controllers/Roles.php
@@ -0,0 +1,15 @@
+<?php
+
+class Roles extends Controllers
+{
+   public function __construct()
+   {
+       parent::__construct();
+   }
+
+   public function index()
+   {
+       $data['page_name'] = "Roles de usuarios";
+       $this->views->getView($this,'index',$data);
+   }
+}
